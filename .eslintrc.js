66ac7c3c5da7641bb4c4ec5e5323b8d736b48c73
nonseodion <nonseifebhor@gmail.com>
2023-01-31 12:47:00 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["airbnb", "airbnb-typescript", "prettier"],
+  plugins: ["import", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+}
