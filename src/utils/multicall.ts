f7d4d8b814d34f3cec3ff09013ea79987cee7d70
nonseodion <nonseifebhor@gmail.com>
2023-03-04 00:19:51 +0100

    setup uniswap redux-multicall

--- src/utils/multicall.ts
+++
@@ -1,4 +0,0 @@
-import { createMulticall } from "@uniswap/redux-multicall"
-
-// Somewhere in your app
-export const multicall = createMulticall({ reducerPath: "multicall" })
---
+++ src/utils/multicall.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { createMulticall } from "@uniswap/redux-multicall"
+import { ChainId } from "./config"
+import getContracts from "./getContracts"
+
+// uses Uniswap multicall for multicalls
+export const multicall = createMulticall({ reducerPath: "multicall" })
+
+export const {
+  useMultipleContractSingleData,
+  useSingleCallResult,
+  useSingleContractMultipleData,
+  useSingleContractWithCallData,
+  useMultiChainMultiContractSingleData,
+  useMultiChainSingleContractSingleData,
+} = multicall.hooks
+
+interface Props {
+  chainId: ChainId
+  blockNumber: number | undefined
+  blocksPerFetch?: number
+}
+
+export function Updater({ chainId, blockNumber, blocksPerFetch }: Props) {
+  const { multicall: multicallContract } = getContracts()
+  const listenerOptions = blocksPerFetch ? { blocksPerFetch } : undefined
+  return (
+    <multicall.Updater
+      chainId={chainId}
+      latestBlockNumber={blockNumber}
+      contract={multicallContract}
+      listenerOptions={listenerOptions}
+    />
+  )
+}
+
+Updater.defaultProps = {
+  blocksPerFetch: 1,
+}
