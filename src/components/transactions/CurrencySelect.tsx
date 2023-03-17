8183b7cd1fa63d18c34d42a638d99f247645f551
Fawaz441 <abdulsalamfawaz4@gmail.com>
2023-03-17 20:25:55 +0100

    Currency dropdowns should have USD and NGN

--- src/components/transactions/CurrencySelect.tsx
+++ src/components/transactions/CurrencySelect.tsx
@@ -7,13 +7,20 @@ import { handleCurrencySelectDropdown } from "../../utils/functions"
 
 type Currency = "USD" | "NGN"
 
-const currencies: Currency[] = ["NGN", "USD"]
+type CurrencyDetail = { name: Currency; fullName: string }
+
+const currencies: CurrencyDetail[] = [
+  { name: "USD", fullName: "US Dollar" },
+  { name: "NGN", fullName: "Nigerian Naira" },
+]
 
 export default function CurrencySelect() {
-  const [selectedCurrency, setSelectedCurrency] = useState<Currency>("NGN")
+  const [selectedCurrency, setSelectedCurrency] = useState<CurrencyDetail>(
+    currencies[0]
+  )
 
-  const onCurrencyChange = (currency: Currency) => {
-    if (selectedCurrency !== currency) {
+  const onCurrencyChange = (currency: CurrencyDetail) => {
+    if (selectedCurrency.name !== currency.name) {
       setSelectedCurrency(currency)
     }
   }
@@ -28,27 +35,45 @@ export default function CurrencySelect() {
         }}
         className="rounded-lg h-6 border border-primary bg-primary/[.15] flex items-center px-[6px]"
       >
-        {selectedCurrency === "USD" ? (
+        {selectedCurrency.name === "USD" ? (
           <USD className="h-4 w-4" />
         ) : (
           <img src={ngn} alt="Nigeria" className="h-4 w-4" />
         )}
         <span className="mx-[2px] mb-[-2px] text-primary font-semibold uppercase text-[10px]">
-          {selectedCurrency}
+          {selectedCurrency.name}
         </span>
         <Dropdown className="h-[10px] w-[10px] mb-[1px]" />
       </button>
-      <ul className="transition-all duration-150 currency-select translate-y-2 opacity-0 pointer-events-none rounded-lg px-3 absolute min-w-[70px] -bottom-[106px] bg-white shadow-sm pt-[18px] pb-[22px] flex flex-col space-y-[18px]">
+      <ul className="transition-all backdrop-blur-[5px] duration-150 currency-select translate-y-2 opacity-0 pointer-events-none rounded-lg px-3 bg-[#292929]/[.62] absolute w-[225px] -bottom-[112px] lg:-bottom-[132px] -left-[70px] shadow-sm pt-[18px] pb-[22px] pl-3 flex flex-col space-y-4">
         {currencies.map((currency) => (
-          <li key={currency}>
+          <li key={currency.name}>
             <button
               onClick={() => onCurrencyChange(currency)}
               type="button"
-              className={clsx("text-sm font-semibold text-lightBlue", {
-                "!text-black": selectedCurrency === currency,
-              })}
+              className="flex items-center space-x-[10px]"
             >
-              {currency}
+              {currency.name === "USD" ? (
+                <USD className="h-4 w-4" />
+              ) : (
+                <img src={ngn} alt="Nigeria" className="h-4 w-4" />
+              )}
+              <div className="flex items-center space-x-3">
+                <span
+                  className={clsx("text-lightBlue text-sm font-semibold", {
+                    "!text-primary": currency.name === selectedCurrency.name,
+                  })}
+                >
+                  {currency.name}
+                </span>
+                <span
+                  className={clsx("text-white/[.4] text-sm", {
+                    "!text-primary": currency.name === selectedCurrency.name,
+                  })}
+                >
+                  {currency.fullName}
+                </span>
+              </div>
             </button>
           </li>
         ))}
